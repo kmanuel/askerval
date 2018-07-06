4a35319c2ca27d798825d0ba1464d96328ad550b
Manuel Kruisz <kruisz88@gmail.com>
2018-07-06 17:52:07 +0200

    resize mainwindow

--- main.js
+++ main.js
@@ -10,9 +10,24 @@ const {app, BrowserWindow, Tray, ipcMain} = require('electron');
 let mainWindow;
 
 function createMainWindow() {
-    mainWindow = new BrowserWindow({width: 275, height: 325, show: false});
+    mainWindow = new BrowserWindow({
+        width: 275,
+        height: 325,
+        show: false,
+        frame: false,
+        resizable: false,
+        webPreferences: {backgroundThrottling: false}
+    });
     mainWindow.loadFile('index.html');
     mainWindow.hide();
+}
+
+function toggleWindowVisibility() {
+    if (mainWindow.isVisible()) {
+        mainWindow.hide();
+    } else {
+        mainWindow.show();
+    }
 }
 
 function init() {
@@ -21,14 +36,14 @@ function init() {
     const iconPath = path.join(__dirname, `./src/assets/question_mark.jpeg`);
     const tray = new Tray(iconPath);
     tray.setToolTip('Don\'t call me. I\'ll call you!');
-    tray.on('click', () => mainWindow.show());
+    tray.on('click', toggleWindowVisibility);
     tray.on('right-click', () => console.log('right-glick'));
 }
 
 app.on('ready', init);
 
 app.on('mainWindowdow-all-closed', () => {
-    if (process.platform !== 'darmainWindow') {
+    if (process.platform !== 'darwin') {
         app.quit();
     }
 });
@@ -43,12 +58,11 @@ ipcMain.on('rating', (event, rating) => {
     db.insert({
         rating,
         date: new Date()
-    }, (err, newDoc) => {
+    }, (err) => {
         if (err) {
             console.log('error inserting rating', err);
-        } else {
-            console.log('inserted new rating', newDoc);
         }
+        mainWindow.hide();
     });
 });
 
