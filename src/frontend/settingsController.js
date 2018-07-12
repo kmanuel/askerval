76c5e9c75a670651288af497f56c02b3e2e637a4
Manuel Kruisz <kruisz88@gmail.com>
2018-07-12 06:18:40 +0200

    remove debug output

--- src/frontend/settingsController.js
+++ src/frontend/settingsController.js
@@ -22,7 +22,6 @@ const create = () => {
 
 
     const show = function () {
-        console.log('showing settings view');
         document.querySelectorAll('.nav-item').forEach(item => item.classList.remove('active'));
         document.querySelectorAll('.toggle-view').forEach(view => view.style.display = 'none');
         document.querySelector('#settings-nav').classList.add('active');
